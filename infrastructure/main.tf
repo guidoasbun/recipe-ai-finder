c93033f32ecb55bdefe6ffb7032e269e6c1be002
guidoasbun <guido@asbun.io>
2026-04-15 15:06:11 -0700

    main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -279,6 +279,7 @@ resource "aws_route53_record" "www" {
 resource "aws_ecr_repository" "app" {
   name                 = var.app_name
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
